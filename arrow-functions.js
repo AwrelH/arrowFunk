9a88ebd14fc597293e9aaba4dd90d3a797ee795f
AwrelH <hiwa.awrel@gmail.com>
2022-08-22 11:56:36 +0000

    different set of function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,38 @@
  */
 
 // Vanilla JavaScript Function
-
+// function addTwoNumber(num1, num2) {
+//     //Code block
+//     return num1 + num2
+// }
+// let sum = (5,3)
+// console.log(sum)
 // Arrow Function With Parameters
+// const addTwoNumber = (num1,num2) => {
+//     return num1 + num2
+// } 
+// let sum = addTwoNumber(5,3);
+// console.log(sum)
 
 // Single Line Arrow Function With Parameters
+const addTwoNumber2 = (num1,num2) => num1+num2;
+let sum = addTwoNumber2(5,3);
+console.log(sum)
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('hello')
+
+// if function without no parantheses 
+const saySomething2 =  () => console.log('hello2');
+saySomething2()
 // Returning Multiple Lines
+const multiLine = () => (
+    `
+    Hello
+    Hello
+    Hello
+    `
+)
+console.log(multiLine())
